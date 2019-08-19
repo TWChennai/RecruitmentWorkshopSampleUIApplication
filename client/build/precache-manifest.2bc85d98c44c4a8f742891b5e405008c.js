c96406d6aa25397db0a98f4a7e5cd44077283f77
Sriram <sriramsb@thoughtworks.com>
2019-08-19 14:36:57 +0530

    Fixing build

---
+++ client/build/precache-manifest.2bc85d98c44c4a8f742891b5e405008c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4096df7efadf0eea425ba0a800ae7419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ffc47bc8a19c29910c9",
+    "url": "/static/css/main.b5ecdb21.chunk.css"
+  },
+  {
+    "revision": "5c8a2a79e6d81ee5ffaa",
+    "url": "/static/js/2.72140aac.chunk.js"
+  },
+  {
+    "revision": "5ffc47bc8a19c29910c9",
+    "url": "/static/js/main.a88ee5ad.chunk.js"
+  },
+  {
+    "revision": "beafcdf2db0a0ea08e8d",
+    "url": "/static/js/runtime~main.e8f1423d.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/icons.1dc35d25.ttf"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/icons.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/icons.c8ddf1e5.woff"
+  },
+  {
+    "revision": "d7c639084f684d66a1bc66855d193ed8",
+    "url": "/static/media/icons.d7c63908.svg"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/icons.e6cf7c6e.woff2"
+  }
+]);
